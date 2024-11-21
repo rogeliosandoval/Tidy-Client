48808c07e86786592e12057ba43981b850a6b88d
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-11-21 10:17:27 -0600

    create signup and login pages

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tc-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class Login {
+
+}
---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tc-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+export class Signup {
+
+}

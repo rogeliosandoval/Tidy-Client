a7a1bb3978cc470b6a563823076009ec58e1dbd0
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-11-21 10:18:53 -0600

    create routes and update the main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Navbar } from './components/navbar/navbar.component';
+import { Component, inject } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { Navbar } from './components/navbar/navbar.component'
+import { SharedService } from './services/shared.service'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    Navbar
+    Navbar,
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tidy-client';
+  public sharedService = inject(SharedService)
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { Home } from './pages/home/home.component'
+import { Signup } from './pages/signup/signup.component'
+import { Login } from './pages/login/login.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: Home,
+    pathMatch: 'full'
+  },
+  {
+    path: 'signup',
+    component: Signup
+  },
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: '**',
+    component: Home
+  }
+]

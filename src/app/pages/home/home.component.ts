56c48dc6f5c139041a239fd3b1a20118a94cfeff
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-11-23 13:20:03 -0600

    add testimonials to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { SharedService } from '../../services/shared.service'
+import { Navbar } from '../../components/navbar/navbar.component'
 
 @Component({
   selector: 'tc-home',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    Navbar
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'

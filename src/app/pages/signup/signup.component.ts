7d306e3203b156429ec6a52be6524ee829ba9742
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-11-24 15:28:56 -0600

    create register page

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core'
+import { InputTextModule } from 'primeng/inputtext'
+import { ButtonModule } from 'primeng/button'
+import { PrimeNGConfig } from 'primeng/api'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'tc-signup',
   standalone: true,
-  imports: [],
+  imports: [
+    InputTextModule,
+    ButtonModule,
+    RouterLink
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class Signup {
+export class Signup implements OnInit {
+  public primengConfig = inject(PrimeNGConfig)
 
+  ngOnInit(): void {
+    this.primengConfig.ripple = true
+  }
 }

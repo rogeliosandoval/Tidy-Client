f10a53963785274e272256d9915bc4799b3b1e6b
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-12-01 18:24:21 -0600

    create notifications page

---
+++ src/app/pages/dashboard/notifications/notifications.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tc-notifications',
+  standalone: true,
+  imports: [],
+  templateUrl: './notifications.component.html',
+  styleUrl: './notifications.component.scss'
+})
+export class Notifications {
+
+}

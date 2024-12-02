2bc73bf8eb32f16d29a39d3bca65907f377108a7
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-12-01 18:24:54 -0600

    crate profits page

---
+++ src/app/pages/dashboard/profits/profits.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tc-profits',
+  standalone: true,
+  imports: [],
+  templateUrl: './profits.component.html',
+  styleUrl: './profits.component.scss'
+})
+export class Profits {
+
+}

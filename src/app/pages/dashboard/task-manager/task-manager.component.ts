11d0a896d0982ae004f885df1bb336ba6f52108e
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-12-02 15:20:19 -0600

    create task manager page

---
+++ src/app/pages/dashboard/task-manager/task-manager.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tc-task-manager',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-manager.component.html',
+  styleUrl: './task-manager.component.scss'
+})
+export class TaskManager {
+
+}

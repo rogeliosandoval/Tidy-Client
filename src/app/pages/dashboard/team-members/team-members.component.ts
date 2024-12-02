a2da47765b713a75f6e3ef0ea241f7d3450b58a6
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-12-02 15:20:33 -0600

    create team members page

---
+++ src/app/pages/dashboard/team-members/team-members.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tc-team-members',
+  standalone: true,
+  imports: [],
+  templateUrl: './team-members.component.html',
+  styleUrl: './team-members.component.scss'
+})
+export class TeamMembers {
+
+}

c4c1e4d3f7c307b9bc6765f6a9962088153d3a6e
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-12-01 18:24:04 -0600

    create client page

---
+++ src/app/pages/dashboard/clients/clients.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tc-clients',
+  standalone: true,
+  imports: [],
+  templateUrl: './clients.component.html',
+  styleUrl: './clients.component.scss'
+})
+export class Clients {
+
+}

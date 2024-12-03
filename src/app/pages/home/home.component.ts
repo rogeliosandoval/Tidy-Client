37f79856d25d3655d0e8456a640f4ab8be3a2c46
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-12-03 16:57:46 -0600

    update the home page design

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,13 +2,17 @@ import { Component, inject } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { SharedService } from '../../services/shared.service'
 import { Navbar } from '../../components/navbar/navbar.component'
+import { RouterLink } from '@angular/router'
+import { Footer } from '../../components/footer/footer.component'
 
 @Component({
   selector: 'tc-home',
   standalone: true,
   imports: [
     CommonModule,
-    Navbar
+    Navbar,
+    Footer,
+    RouterLink
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'

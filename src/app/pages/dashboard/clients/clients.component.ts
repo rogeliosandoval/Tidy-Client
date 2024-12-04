80eeaa99462be9f59e47a68d8d5a200d38eac6a2
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-12-04 16:02:31 -0600

    first design clients page

--- src/app/pages/dashboard/clients/clients.component.ts
+++ src/app/pages/dashboard/clients/clients.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core'
+import { SharedService } from '../../../services/shared.service'
+import { MenuItem } from 'primeng/api'
+import { MenuModule } from 'primeng/menu'
 
 @Component({
   selector: 'tc-clients',
   standalone: true,
-  imports: [],
+  imports: [
+    MenuModule
+  ],
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.scss'
 })
-export class Clients {
+export class Clients implements OnInit {
+  public sharedService = inject(SharedService)
+  public clientOptions: MenuItem[] | undefined
 
+  ngOnInit(): void {
+    this.clientOptions = [
+      {
+        label: 'View Details',
+        icon: 'pi pi-info-circle'
+      },
+      {
+        label: 'Edit',
+        icon: 'pi pi-pencil'
+      },
+      {
+        label: 'Create Note',
+        icon: 'pi pi-pen-to-square'
+      },
+      {
+        label: 'Send Email',
+        icon: 'pi pi-envelope'
+      },
+      {
+        label: 'Delete',
+        icon: 'pi pi-trash'
+      }
+    ]
+  }
 }

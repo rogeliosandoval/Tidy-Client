6097161cf7cf28a9c9d2dee619bd30296b101b33
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-12-05 00:28:56 -0600

    update profile page, design

--- src/app/pages/dashboard/profile/profile.component.ts
+++ src/app/pages/dashboard/profile/profile.component.ts
@@ -1,13 +1,19 @@
 import { Component, inject } from '@angular/core'
 import { SharedService } from '../../../services/shared.service'
+import { AuthService } from '../../../services/auth.service'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'tc-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
+
 export class Profile {
   public sharedService = inject(SharedService)
+  public authService = inject(AuthService)
 }

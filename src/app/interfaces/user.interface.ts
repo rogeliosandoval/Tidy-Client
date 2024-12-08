950f3cbf42a5c79286bccc7dbfd89ad0a83ebaa7
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-12-07 18:48:55 -0600

    update dashboard with dynamic data

--- src/app/interfaces/user.interface.ts
+++ src/app/interfaces/user.interface.ts
@@ -2,3 +2,9 @@ export interface UserInterface {
   email: string
   name: string
 }
+
+export interface CoreUserInterface {
+  name: string
+  email: string
+  business_name: string
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -11,7 +11,13 @@ import { ProgressSpinnerModule } from 'primeng/progressspinner'
 import { SidebarModule } from 'primeng/sidebar'
 import { ButtonModule } from 'primeng/button'
 import { DialogModule } from 'primeng/dialog'
-import { FormsModule, ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms'
+import { PrimeNGConfig } from 'primeng/api'
+import { FormsModule, ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms'
+import { doc, Firestore, setDoc } from '@angular/fire/firestore'
+import { ToastModule } from 'primeng/toast'
+import { MessageService } from 'primeng/api'
+import { take } from 'rxjs'
+import { v4 as uuidv4 } from 'uuid'
 
 @Component({
   selector: 'tc-dashboard',
@@ -25,33 +31,37 @@ import { FormsModule, ReactiveFormsModule, FormControl, FormGroup } from '@angul
     RouterOutlet,
     SidebarModule,
     ButtonModule,
-    DialogModule
+    DialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastModule
+  ],
+  providers: [
+    MessageService
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class Dashboard implements OnInit {
+  private messageService = inject(MessageService)
+  private firestore = inject(Firestore)
   private router = inject(Router)
+  private uid = signal<any>('')
+  public primengConfig = inject(PrimeNGConfig)
   public authService = inject(AuthService)
   public sharedService = inject(SharedService)
   public items: MenuItem[] | undefined
   public currentRoute = signal<string>('')
   public sidebarVisible = signal<boolean>(false)
   public showStartupModal: boolean = false
-  public fetchingData = signal<boolean>(true)
-  public userData = signal<any>(undefined)
+  public modalLoading = signal<boolean>(false)
   public startupForm = new FormGroup({
-    
+    business_name: new FormControl('', Validators.required)
   })
 
   ngOnInit(): void {
-    this.authService.fetchCoreUserData()
-    .then((data: any) => {
-      this.userData.set(data)
-    })
-    .then(() => {
-      this.fetchingData.set(false)
-    })
+    this.primengConfig.ripple = true
+    this.initializeApp()
 
     this.items = [
       {
@@ -67,7 +77,10 @@ export class Dashboard implements OnInit {
       },
       {
         label: 'Account Settings',
-        icon: 'pi pi-cog'
+        icon: 'pi pi-cog',
+        command: () => {
+          this.router.navigateByUrl('/dashboard/account-settings')
+        }
       },
       {
         label: 'Sign Off',
@@ -79,8 +92,73 @@ export class Dashboard implements OnInit {
     ]
   }
 
+  public async initializeApp(): Promise<void> {
+    this.sharedService.loading.set(true)
+    await this.authService.fetchCoreUserData()
+    await this.authService.fetchProfileAvatar()
+    await this.authService.fetchBusinessAvatar()
+    await this.authService.fetchCoreBusinessData()
+    .then(() => {
+      if (!this.authService.coreUserData().business_id) {
+        this.showStartupModal = true
+      }
+    })
+    .then(() => {
+      this.sharedService.loading.set(false)
+    })
+  }
+
   public grabRoute() {
     return this.router.url
+  }
+
+  public async saveBusinessName() {
+    this.modalLoading.set(true)
+    const businessName = this.startupForm.get('business_name')?.value
+    this.authService.user$
+    .pipe(take(1))
+    .subscribe({
+      next: async (data: any) => {
+        if (data && data.uid) {
+          const uid = data.uid
+          const businessId = uuidv4()
+          const userRef = doc(this.firestore, `users/${uid}`)
+          const businessRef = doc(this.firestore, `businesses/${businessId}`)
+
+          await setDoc(businessRef, {
+            id: businessId,
+            name: businessName,
+            clients: 0,
+            members: 1, // Initial member count
+            owner: uid, // Reference the owner
+          })
+
+          await setDoc(userRef, { business_id: businessId }, { merge: true })
+
+          await this.authService.fetchCoreUserData()
+
+          await this.authService.fetchCoreBusinessData()
+          .then(() => {
+            this.showStartupModal = false
+          })
+          .then(() => {
+            this.startupForm.reset()
+            this.modalLoading.set(false)
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Success',
+              detail: 'Business name has been added!',
+              key: 'br',
+              life: 4000,
+            })
+          })
+        }
+      },
+      error: (err: any) => {
+        console.error(err)
+        this.modalLoading.set(false)
+      },
+    })
   }
 
   public signOff(): void {

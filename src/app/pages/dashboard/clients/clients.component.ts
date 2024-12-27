1ec30f3ce5bad5f7d3c4d2cf9f4b8aa872259877
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-12-27 01:01:32 -0600

    update clients page, begin logic and main style

--- src/app/pages/dashboard/clients/clients.component.ts
+++ src/app/pages/dashboard/clients/clients.component.ts
@@ -1,23 +1,36 @@
-import { Component, OnInit, inject } from '@angular/core'
+import { Component, OnInit, inject, signal } from '@angular/core'
 import { SharedService } from '../../../services/shared.service'
+import { AuthService } from '../../../services/auth.service'
 import { MenuItem } from 'primeng/api'
 import { MenuModule } from 'primeng/menu'
+import { ConfirmDialog } from '../../../dialogs/confirm/confirm.component'
+import { TruncatePipe } from '../../../pipes/truncate.pipe'
+import { TooltipModule } from 'primeng/tooltip'
+import { ButtonModule } from 'primeng/button'
 
 @Component({
   selector: 'tc-clients',
   standalone: true,
   imports: [
-    MenuModule
+    MenuModule,
+    ConfirmDialog,
+    TruncatePipe,
+    TooltipModule,
+    ButtonModule
   ],
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.scss'
 })
 export class Clients implements OnInit {
   public sharedService = inject(SharedService)
+  public authService = inject(AuthService)
+  public showConfirmModal = signal<boolean>(false)
   public clientOptions: MenuItem[] | undefined
+  public modalMessage = signal<string>('')
+  public modalClientName = signal<any>(null)
 
   ngOnInit(): void {
-    this.clientOptions = [
+    this.clientOptions =  [
       {
         label: 'View Details',
         icon: 'pi pi-info-circle'
@@ -36,8 +49,20 @@ export class Clients implements OnInit {
       },
       {
         label: 'Delete',
-        icon: 'pi pi-trash'
+        icon: 'pi pi-trash',
+        command: () => {
+          this.openConfirmModal(`Are you sure you want to delete this client? (${this.modalClientName()})`)
+        }
       }
     ]
+  }
+
+  public onModalClose(newState: boolean) {
+    this.showConfirmModal.set(newState)
+  }
+
+  public openConfirmModal(message: string): void {
+    this.modalMessage.set(message)
+    this.showConfirmModal.set(true)
   }
 }

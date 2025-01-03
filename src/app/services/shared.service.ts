275348b2e7b09145c5a942b9b48d0e0f322a150a
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-01-03 00:06:10 -0600

    add variable to shared service

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -7,6 +7,7 @@ import { Injectable, signal } from '@angular/core'
 export class SharedService {
   darkMode = signal<boolean>(false)
   loading = signal<boolean>(false)
+  showAvatarUploadDialog = signal<boolean>(false)
   showClientFormDialog = signal<boolean>(false)
   clientFormType = signal<string>('')
   dialogClient = signal<any>(null)

6682969501f936b8df9c66246f8ce3e8f6f72459
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-01-20 19:49:25 -0600

    create contact list dialog

---
+++ src/app/dialogs/contact-list/contact-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'tcd-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrl: './contact-list.component.scss'
+})
+
+export class ContactListDialog {
+
+}

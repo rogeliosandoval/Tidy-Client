d3a1dd7f8b3952dec375b5a918dd7a2ff0b946ae
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-01-21 22:28:41 -0600

    begin logic for contact list dialog

--- src/app/dialogs/contact-list/contact-list.component.ts
+++ src/app/dialogs/contact-list/contact-list.component.ts
@@ -1,11 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output, inject, input, signal } from '@angular/core'
+import { DialogModule } from 'primeng/dialog'
+import { ButtonModule } from 'primeng/button'
+import { ProgressSpinnerModule } from 'primeng/progressspinner'
 
 @Component({
   selector: 'tcd-contact-list',
+  standalone: true,
+  imports: [
+    DialogModule,
+    ButtonModule,
+    ProgressSpinnerModule
+  ],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.scss'
 })
 
 export class ContactListDialog {
+  @Input() showContactListDialog = false
+  @Output() onClose = new EventEmitter<boolean>()
+  @Output() onSubmit = new EventEmitter<any>()
+  public dialogLoading = input<boolean>()
 
+  public closeDialog() {
+    this.showContactListDialog = false
+    this.onClose.emit(false)
+  }
+
+  public submitDialog(): void {
+    this.onSubmit.emit(true)
+  }
 }
